0709e08aff2468a37f89b0003bf1ba84a3114d90
Andreas Bannach <andreas@borntohula.de>
2015-07-06 22:53:45 +0200

    test for array values

--- test/test.js
+++ test/test.js
@@ -1,10 +1,18 @@
 describe('java-properties-loader', function () {
 
-  it('should return a JSON object', function () {
+  it('should handle basic string values', function () {
     var properties = require('!json!../!./fixtures/basic.resources');
     var expected = require('!json!./fixtures/basic.json');
 
     (typeof properties).should.be.equal('object');
+    properties.should.eql(expected);
+  });
+
+  it('should handle array values', function () {
+    var properties = require('!json!../!./fixtures/array.resources');
+    var expected = require('!json!./fixtures/array.json');
+
+    (Array.isArray(properties['a.key'])).should.be.eql(true);
     properties.should.eql(expected);
   });
 
